8300eda2aaf102f84ff2a8914d48db7aa82c777b
ridhoarrosyid <muhammadridhoarrosyid@gmail.com>
2024-05-23 10:18:45 +0700

    menambahkan file materi 4 dan tugas parkir

---
+++ tugas/tugas_parkir.php
@@ -0,0 +1,52 @@
+<?php
+$nama = 'Micheal';
+$nama = 10;
+
+$pertama = 5;
+$kedua = 10;
+
+$jumlah = $pertama + $kedua;
+
+$dariFunction = penjumlahan(60, 80);
+
+function penjumlahan($a, $b)
+{
+    return $a + $b;
+}
+
+function tarifParkir($tarif,$harga)
+{
+    return $tarif*$harga;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="#" method="get">
+        No Polisi : <input type="text" name="pertama" /> <br>
+        Angka Kedua : <input type="number" name="kedua" /> <br>
+        <input type="submit" name="btnHitung" value="hitung tarif" />
+    </form>
+
+    <?php
+        // mengambil data dari URL GET
+        if (isset($_GET['btnHitung'])) {
+            $angka_pertama = $_GET['pertama'];
+            $angka_kedua = $_GET['kedua'];
+
+            $hasil = penjumlahan($angka_pertama, $angka_kedua);
+            echo " tarif parkir = ";
+            echo tarifParkir($hasil,2000);
+        }
+        ?>
+</body>
+
+</html>
